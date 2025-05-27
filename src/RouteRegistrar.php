e69c4eb995ce6b76e1f75d8a56a3ca49a81e0897
jundayw <jundayw@126.com>
2025-05-27 18:23:35 +0800

    RouteRegistrar

--- src/RouteRegistrar.php
+++ src/RouteRegistrar.php
@@ -2,17 +2,344 @@
 
 namespace Annotation\Routing;
 
+use Annotation\Route\Contracts\RoutingContract;
+use Illuminate\Routing\PendingResourceRegistration;
+use Illuminate\Routing\PendingSingletonResourceRegistration;
 use Illuminate\Routing\Router;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use ReflectionAttribute;
+use ReflectionClass;
+use ReflectionMethod;
+use SplFileInfo;
+use Symfony\Component\Finder\Finder;
 
 class RouteRegistrar
 {
+    protected string $basePath;
+
+    protected string $rootNamespace;
+
+    protected array $middleware        = [];
+    protected array $withoutMiddleware = [];
+
     public function __construct(protected Router $router)
     {
-        //
+        $this->useBasePath(app()->path());
     }
 
-    public function directories(array $directories)
+    /**
+     * @param string $basePath
+     *
+     * @return $this
+     */
+    public function useBasePath(string $basePath): self
     {
-        //
+        $this->basePath = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $basePath);
+
+        return $this;
+    }
+
+    /**
+     * @param string $rootNamespace
+     *
+     * @return $this
+     */
+    public function useRootNamespace(string $rootNamespace): self
+    {
+        $this->rootNamespace = rtrim(str_replace('/', '\\', $rootNamespace), '\\') . '\\';
+
+        return $this;
+    }
+
+    /**
+     * @param string|array $middleware
+     *
+     * @return $this
+     */
+    public function useMiddleware(string|array $middleware): self
+    {
+        $this->middleware = Arr::wrap($middleware);
+
+        return $this;
+    }
+
+    public function middleware(): array
+    {
+        return $this->middleware ?? [];
+    }
+
+    /**
+     * @param string|array $withoutMiddleware
+     *
+     * @return $this
+     */
+    public function useWithoutMiddleware(string|array $withoutMiddleware): self
+    {
+        $this->withoutMiddleware = Arr::wrap($withoutMiddleware);
+
+        return $this;
+    }
+
+    public function withoutMiddleware(): array
+    {
+        return $this->withoutMiddleware ?? [];
+    }
+
+    /**
+     * @param string|array $directories
+     * @param array        $patterns
+     * @param array        $notPatterns
+     *
+     * @return void
+     */
+    public function registerDirectory(string|array $directories, array $patterns = [], array $notPatterns = []): void
+    {
+        $directories = Arr::wrap($directories);
+        $patterns    = $patterns ?: ['*.php'];
+
+        $files = (new Finder())->files()->in($directories)->name($patterns)->notName($notPatterns)->sortByName();
+
+        collect($files)->each(fn(SplFileInfo $file) => $this->registerFile($file));
+    }
+
+    /**
+     * @param string|SplFileInfo $path
+     *
+     * @return void
+     */
+    public function registerFile(string|SplFileInfo $path): void
+    {
+        if (is_string($path)) {
+            $path = new SplFileInfo($path);
+        }
+
+        $fullyQualifiedClassName = $this->fullQualifiedClassNameFromFile($path);
+
+        $this->processAttributes($fullyQualifiedClassName);
+    }
+
+    /**
+     * @param SplFileInfo $file
+     *
+     * @return string
+     */
+    protected function fullQualifiedClassNameFromFile(SplFileInfo $file): string
+    {
+        $class = trim(Str::replaceFirst($this->basePath, '', $file->getRealPath()), DIRECTORY_SEPARATOR);
+
+        $class = str_replace(
+            [DIRECTORY_SEPARATOR, 'App\\'],
+            ['\\', app()->getNamespace()],
+            Str::replaceLast('.php', '', $class)
+        );
+
+        return $this->rootNamespace . $class;
+    }
+
+    /**
+     * @param string $class
+     *
+     * @return void
+     */
+    public function registerClass(string $class): void
+    {
+        $this->processAttributes($class);
+    }
+
+    /**
+     * @param string $className
+     *
+     * @return void
+     */
+    protected function processAttributes(string $className): void
+    {
+        if (!class_exists($className)) {
+            return;
+        }
+
+        $class = new ReflectionClass($className);
+
+        $routeAttributes = new RouteAttributes($class);
+
+        $this->registerRoutes($class, $routeAttributes);
+    }
+
+    /**
+     * @param ReflectionClass $class
+     * @param RouteAttributes $routeAttributes
+     *
+     * @return void
+     */
+    protected function registerRoutes(ReflectionClass $class, RouteAttributes $routeAttributes): void
+    {
+        foreach ($routeAttributes->groups() as $group) {
+            $this->group($group, fn() => $this->getRoutes($class, $routeAttributes));
+        }
+    }
+
+    /**
+     * @param ReflectionClass $class
+     * @param RouteAttributes $routeAttributes
+     *
+     * @return RouteMethodAttributes[]
+     */
+    protected function getDeclaringMethods(ReflectionClass $class, RouteAttributes $routeAttributes): array
+    {
+        $methods = array_filter($class->getMethods(ReflectionMethod::IS_PUBLIC), function (ReflectionMethod $method) use ($class) {
+            $attributes = $method->getAttributes(RoutingContract::class, ReflectionAttribute::IS_INSTANCEOF);
+            if (count($attributes) === 0) {
+                return false;
+            }
+            if ($method->getDeclaringClass()->getName() == $class->getName()) {
+                return true;
+            }
+            return false;
+        });
+        return array_map(function (ReflectionMethod $method) use ($class, $routeAttributes) {
+            return new RouteMethodAttributes($class, $routeAttributes, $method);
+        }, $methods);
+    }
+
+    /**
+     * @param ReflectionClass $class
+     * @param RouteAttributes $routeAttributes
+     *
+     * @return void
+     */
+    public function getRoutes(ReflectionClass $class, RouteAttributes $routeAttributes): void
+    {
+        if ($routeAttributes->isResourceContract()) {
+            $this->getResourceRoutes($class, $routeAttributes);
+            return;
+        }
+
+        foreach ($this->getDeclaringMethods($class, $routeAttributes) as $method) {
+            foreach ($method->getRouteAttributes() as $attribute) {
+                $route = $this->router
+                    ->addRoute($attribute->methods, $attribute->uri, $method->getRouteAction())
+                    ->name($attribute->name ?? $method->getRouteName());
+                $method->setScopeBindingsIfAvailable($route)
+                    ->setWithTrashedIfAvailable($route)
+                    ->setWheresIfAvailable($route)
+                    ->setDefaultsIfAvailable($route)
+                    ->addMiddlewareToRoute($route, $this->middleware())
+                    ->addWithoutMiddlewareToRoute($route, $this->withoutMiddleware());
+            }
+        }
+    }
+
+    /**
+     * @param ReflectionClass $class
+     * @param RouteAttributes $routeAttributes
+     *
+     * @return PendingResourceRegistration
+     */
+    protected function getRouteForResource(ReflectionClass $class, RouteAttributes $routeAttributes): PendingResourceRegistration
+    {
+        return $routeAttributes->apiResource()
+            ? $this->router->apiResource($routeAttributes->resource(), $class->getName())
+            : $this->router->resource($routeAttributes->resource(), $class->getName());
+    }
+
+    /**
+     * @param ReflectionClass $class
+     * @param RouteAttributes $routeAttributes
+     *
+     * @return PendingSingletonResourceRegistration
+     */
+    protected function getRouteForSingletonResource(ReflectionClass $class, RouteAttributes $routeAttributes): PendingSingletonResourceRegistration
+    {
+        return $routeAttributes->apiSingleton()
+            ? $this->router->apiSingleton($routeAttributes->singleton(), $class->getName())
+            : $this->router->singleton($routeAttributes->singleton(), $class->getName());
+    }
+
+    /**
+     * @param ReflectionClass $class
+     * @param RouteAttributes $routeAttributes
+     *
+     * @return void
+     */
+    public function getResourceRoutes(ReflectionClass $class, RouteAttributes $routeAttributes): void
+    {
+        $route = match (true) {
+            $routeAttributes->isSingleton() => $this->getRouteForSingletonResource($class, $routeAttributes),
+            default => $this->getRouteForResource($class, $routeAttributes)
+        };
+
+        $methods = [
+            'only',
+            'except',
+            'names',
+            'parameters',
+            'shallow',
+        ];
+
+        foreach ($methods as $method) {
+            if (!method_exists($route, $method)) {
+                continue;
+            }
+            if (!is_null($value = call_user_func([$routeAttributes, $method]))) {
+                call_user_func([$route, $method], $value);
+            }
+        }
+
+        $methods = [
+            'creatable',
+            'destroyable',
+        ];
+
+        foreach ($methods as $method) {
+            if (!method_exists($route, $method)) {
+                continue;
+            }
+            if (call_user_func([$routeAttributes, $method]) === true) {
+                call_user_func([$route, $method]);
+            }
+        }
+
+        $route->middleware(array_merge(
+            $this->middleware,
+            $routeAttributes->middleware()
+        ))->withoutMiddleware(array_merge(
+            $this->withoutMiddleware,
+            $routeAttributes->withoutMiddleware()
+        ));
+    }
+
+    /**
+     * @param array $directories
+     *
+     * @return void
+     */
+    public function directories(array $directories): void
+    {
+        foreach ($directories as $directory => $attributes) {
+            if (is_string($attributes)) {
+                [$directory, $attributes] = [$attributes, []];
+            }
+            $options       = Arr::except($attributes, ['namespace', 'base_path', 'patterns', 'not_patterns']);
+            $rootNamespace = $attributes['namespace'] ?? app()->getNamespace();
+            $basePath      = $attributes['base_path'] ?? (isset($attributes['namespace']) ? $directory : app()->path());
+            $patterns      = $attributes['patterns'] ?? [];
+            $notPatterns   = $attributes['not_patterns'] ?? [];
+            $this->useRootNamespace($rootNamespace)
+                ->useBasePath($basePath)
+                ->group($options, fn() => $this->registerDirectory($directory, $patterns, $notPatterns));
+        }
+    }
+
+    /**
+     * @param array    $options
+     * @param callable $routes
+     *
+     * @return $this
+     */
+    public function group(array $options, callable $routes): self
+    {
+        $this->router->group($options, $routes);
+
+        return $this;
     }
 }
